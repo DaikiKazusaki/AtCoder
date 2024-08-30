79b3e1f3eb2c5fa2edc17ae3e94538ee40529197
DaikiKazusaki <130907832+DaikiKazusaki@users.noreply.github.com>
2024-08-30 18:50:09 +0900

    Delete Beginner_343 directory

--- Beginner_343/A - Wrong Answer.py
+++
@@ -1,6 +0,0 @@
-a, b = map(int, input().split())
-
-for _ in range(1, 9):
-    if _ != a + b:
-        print(_)
-        break
--- Beginner_343/B - Adjacency Matrix.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-A = [list(map(int, input().split())) for _ in range(n)]
-
-for i in range(n):
-    for j in range(n):
-        if A[i][j] == 1:
-            print(j + 1, end = ' ')
-    print() 
