bbd3c27e00968f5347918fc105a5264f02487de9
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-09-02 18:45:04 +0900

    C修正完了

--- ABC369/C - Count Arithmetic Subarrays.py
+++ ABC369/C - Count Arithmetic Subarrays.py
@@ -13,7 +13,8 @@ for i in range(1, len(sub_a)):
         num_sub_a.append(count)
         count = 1
 
-num_sub_a.append(count)
+if n != 1:
+    num_sub_a.append(count)
 
 for i in num_sub_a:
     result += i * (i + 1) // 2
