ff04bf17b1476c9eb100d9fd0fee111a495c45c4
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-09-12 21:33:47 +0900

    Cほぼ完了

--- ABC370/C - Word Ladder.py
+++ ABC370/C - Word Ladder.py
@@ -1,14 +1,17 @@
 s = list(input())
 t = list(input())
+n = len(s)
 x = list()
 
-sorted_t = sorted(t)
-
-print(sorted_t)
-
-for item in sorted_t:
-    index = t.index(item)
-    if s[index] != item:
-        x.append(s)
+while s != t:
+    nxt = 'z' * n
+    for i in range(n):
+        if s[i] != t[i]:
+            tmp = s[:i] + t[i] + s[i + 1:]
+            nxt = min(nxt, tmp)
+    x.append(nxt)
+    s = nxt
     
-print(x)
+print(len(x))
+for _ in x:
+    print(_)
