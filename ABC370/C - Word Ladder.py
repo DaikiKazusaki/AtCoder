8b11ffedcf000acd0e7710d20a767054bed853f2
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-09-12 21:42:46 +0900

    C完了

--- ABC370/C - Word Ladder.py
+++ ABC370/C - Word Ladder.py
@@ -1,17 +1,17 @@
 s = list(input())
 t = list(input())
 n = len(s)
-x = list()
+x = []
 
 while s != t:
-    nxt = 'z' * n
+    nxt = ['z'] * n  # nxtをリストとして初期化
     for i in range(n):
         if s[i] != t[i]:
-            tmp = s[:i] + t[i] + s[i + 1:]
+            tmp = s[:i] + [t[i]] + s[i + 1:]  # t[i]をリストにして結合
             nxt = min(nxt, tmp)
-    x.append(nxt)
+    x.append(''.join(nxt))  # リストを文字列に変換して追加
     s = nxt
-    
+
 print(len(x))
 for _ in x:
     print(_)
