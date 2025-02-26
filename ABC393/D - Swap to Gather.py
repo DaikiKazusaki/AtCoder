1724db6782e241ce36b12177b7cd29ff049f42fb
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-02-26 16:13:27 +0900

    solve D in ABC393

--- ABC393/D - Swap to Gather.py
+++ ABC393/D - Swap to Gather.py
@@ -1,2 +1,13 @@
 n = int(input())
-s = list(input())
+s = input()
+c1 = s.count('1')
+now = 0
+ans = 0
+
+for c in s:
+    if c == '0':
+        ans += min(now, c1 - now)
+    else:
+        now += 1
+
+print(ans)
