7962a97422e0c9d92934cefe1722c0f587588cb4
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-09-06 18:24:54 +0900

    add 420-C

---
+++ ABC420/C - Sum of Min Query.py
@@ -0,0 +1,25 @@
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+cxv = [input().split() for _ in range(q)]
+ans = 0
+
+for i in range(n):
+  ans += min(a[i], b[i])
+
+for i in range(q):
+  c, x, v = cxv[i]
+  x = int(x) - 1
+  v = int(v)
+
+  if c == 'A':
+    ans -= min(a[x], b[x])
+    a[x] = v
+    ans += min(a[x], b[x])
+  else:
+    ans -= min(a[x], b[x])
+    b[x] = v
+    ans += min(a[x], b[x])
+
+  print(ans)
+  
